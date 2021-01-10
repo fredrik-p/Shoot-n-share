0b92738c603d537a15e3a83d35fe70b6bf250cd6
Fredrik Pekkarinen <fredrik.pekkarinen@gmail.com>
2021-01-10 20:09:44 +0100

    Moved navigation to its own component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Navbar, Nav, Container, Button } from 'react-bootstrap'
-import logo from './assets/images/logo.svg'
-import background from './assets/images/bg-home.jpg';
+import { Container, Button } from 'react-bootstrap'
+import Navigation from './components/Navigation'
+import background from './assets/images/bg-home.jpg'
+import logobig from './assets/images/logo.svg'
 import './assets/scss/app.scss'
 
 const App = () => {
 	return (
 		<>
+
 			<div style={{
 				backgroundImage: `url(${background})`,
 				backgroundSize: 'cover',
@@ -14,27 +16,7 @@ const App = () => {
 				backgroundRepeat: 'no-repeat',
 				height: '100vh'
 			}}>
-				<Navbar collapseOnSelect expand="lg" bg="transparent" variant="light">
-					<Container>
-						<Navbar.Brand href="#home">
-							<img
-								alt="logo"
-								src={logo}
-								width="30"
-								height="30"
-								className="d-inline-block align-top"
-							/>{' '}<h5>Shoot n' share</h5>
-						</Navbar.Brand>
-
-						<Navbar.Toggle aria-controls="basic-navbar-nav" />
-						<Navbar.Collapse id="basic-navbar-nav">
-							<Nav className="ml-auto">
-								<Nav.Link href="/login">Log in</Nav.Link>
-								<Nav.Link href="/sign-up">Sign up</Nav.Link>
-							</Nav>
-						</Navbar.Collapse>
-					</Container>
-				</Navbar>
+				<Navigation />
 
 
 				<Container>
@@ -42,7 +24,7 @@ const App = () => {
 						<div className="col-lg-12 mt-5 text-center">
 							<img
 								alt="logo"
-								src={logo}
+								src={logobig}
 								width="100"
 								height="100"
 								className="d-inline-block align-top"
---
+++ src/components/Navigation.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import logo from '../assets/images/logo.svg'
+
+export const Navigation = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="transparent" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt="logo"
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}<h5>Shoot n' share</h5>
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="/login">Log in</Nav.Link>
+                            <Nav.Link href="/sign-up">Sign up</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Navigation
