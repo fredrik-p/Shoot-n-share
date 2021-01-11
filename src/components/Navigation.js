8b2e5b9a71195829a093659e5958f5def83c8df8
Fredrik Pekkarinen <fredrik.pekkarinen@gmail.com>
2021-01-11 12:19:05 +0100

    added NavLink and Link to fix problem with route in Nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { NavLink, Link } from 'react-router-dom'
 import logo from '../assets/images/logo.svg'
 
 export const Navigation = () => {
@@ -7,21 +8,23 @@ export const Navigation = () => {
         <div>
             <Navbar collapseOnSelect expand="lg" bg="transparent" variant="light">
                 <Container>
-                    <Navbar.Brand href="/">
-                        <img
-                            alt="logo"
-                            src={logo}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
-                        />{' '}<h5>Shoot n' share</h5>
-                    </Navbar.Brand>
+                    <Link to="/">
+                        <Navbar.Brand>
+                            <img
+                                alt="logo"
+                                src={logo}
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />{' '}<h5>Shoot n' share</h5>
+                        </Navbar.Brand>
+                    </Link>
 
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <Nav.Link href="/login">Log in</Nav.Link>
-                            <Nav.Link href="/sign-up">Sign up</Nav.Link>
+                            <NavLink to="/login" className="nav-link">Log in</NavLink>
+                            <NavLink to="/sign-up" className="nav-link">Sign up</NavLink>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
